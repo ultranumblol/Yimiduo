dad09834faedaf68c3ca94360e48789c082e0bc8
wgz <490480770@qq.com>
2016-06-17 13:20:49 +0800

    6/17

--- app/src/main/java/com/example/qwerr/yimiduo/WorkerRegistActivity2.java
+++ app/src/main/java/com/example/qwerr/yimiduo/WorkerRegistActivity2.java
@@ -1,10 +1,14 @@
 package com.example.qwerr.yimiduo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class WorkerRegistActivity2 extends AppCompatActivity {
+    private TextView finishRegist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +16,19 @@ public class WorkerRegistActivity2 extends AppCompatActivity {
         setContentView(R.layout.regist_worker2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("师傅注册");
+        init();
     }
+
+    private void init() {
+        finishRegist = (TextView) findViewById(R.id.id_workerregist_finish);
+        finishRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(WorkerRegistActivity2.this,MainActivity.class));
+            }
+        });
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId()==android.R.id.home){
--- app/src/main/java/com/example/qwerr/yimiduo/fragment/Fragment1.java
+++ app/src/main/java/com/example/qwerr/yimiduo/fragment/Fragment1.java
@@ -9,17 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.qwerr.yimiduo.LoginActivity;
 import com.example.qwerr.yimiduo.R;
+import com.example.qwerr.yimiduo.ShifuActivity;
 import com.example.qwerr.yimiduo.WorkerRegistActivity;
+import com.example.qwerr.yimiduo.YuyueActivity;
 
 /**
  * Created by qwerr on 2016/6/7.
  */
 
 public class Fragment1 extends Fragment {
-    private ImageView chuguianzhuang,shafaweixiu;
+    private ImageView chuguianzhuang,shafaweixiu,jiajuanzhuang,diannaoanzhuang,yuyuesonghuo;
 
     @Nullable
     @Override
@@ -32,6 +35,28 @@ public class Fragment1 extends Fragment {
     private void initView(View view) {
         chuguianzhuang = (ImageView) view.findViewById(R.id.id_frag1_chuguianzhuang);
         shafaweixiu = (ImageView) view.findViewById(R.id.id_frag1_shafaweixiu);
+        jiajuanzhuang = (ImageView) view.findViewById(R.id.id_frag1_jiajuanzhuang);
+        yuyuesonghuo = (ImageView) view.findViewById(R.id.id_frag1_yuyuesonghuo);
+        yuyuesonghuo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ShifuActivity.class));
+
+            }
+        });
+        diannaoanzhuang = (ImageView) view.findViewById(R.id.id_frag1_diannaoanzhuang);
+        diannaoanzhuang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ShifuActivity.class));
+            }
+        });
+        jiajuanzhuang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), YuyueActivity.class));
+            }
+        });
         shafaweixiu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
